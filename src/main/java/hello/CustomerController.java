e3be8f6f4cafa07eed7efa5fc217c4e58d1241c2
Jamie Milhoan <jlmilhoan@gmail.com>
2017-06-28 21:42:37 -0400

    first commit

---
+++ src/main/java/hello/CustomerController.java
@@ -0,0 +1,29 @@
+package hello;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class CustomerController {
+
+	
+
+		@Resource
+		CustomerRepository repository;
+
+			
+		@RequestMapping("/home")
+		public String fetchCustomers(Model model) {
+			model.addAttribute("customersAsCollection", repository.findAll());
+			return "home";
+		}
+		
+//		@RequestMapping("/onecustomer")
+//	    public String fetchOne(Model model){
+//	         model.addAttribute("onecustomer", repository.findByLastName(lastName));
+//	         return "onereview";
+		
+}
